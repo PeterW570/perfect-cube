7f8831625a24d5fc94ad63480622503c39d02926
Peter <peter.w570@gmail.com>
2021-03-07 15:34:17 +0000

    ability to add guidelines passing through start & end of each line

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,14 @@
 	let ctx;
 
 	let canvasOffsets = { x: 0, y: 0 };
+	let canvasSize = {};
 	let offsetsSet = false;
 
 	let startPosition = { x: 0, y: 0 };
 	let endPosition = { x: 0, y: 0 };
 	let isDrawing = false;
+
+	let lineHistory = [];
 
 	const getClientOffset = (event) => {
 		const { pageX, pageY } = event.touches ? event.touches[0] : event;
@@ -22,21 +25,30 @@
 		};
 	};
 
-	function drawLine() {
+	function drawLine({ start = startPosition, end = endPosition } = {}) {
 		ctx.beginPath();
-		ctx.moveTo(startPosition.x, startPosition.y);
-		ctx.lineTo(endPosition.x, endPosition.y);
+		ctx.moveTo(start.x, start.y);
+		ctx.lineTo(end.x, end.y);
 		ctx.stroke();
 	}
 
 	function onMouseDown(event) {
 		if (!offsetsSet) {
-			canvasOffsets.x = canvasEl.getBoundingClientRect().left;
-			canvasOffsets.y = canvasEl.getBoundingClientRect().top;
+			const {
+				left,
+				top,
+				height,
+				width,
+			} = canvasEl.getBoundingClientRect();
+			canvasOffsets.x = left;
+			canvasOffsets.y = top;
+			canvasSize.height = height;
+			canvasSize.width = width;
 		}
 
 		startPosition = getClientOffset(event);
 		isDrawing = true;
+		lineHistory.push([startPosition]);
 	}
 
 	function onMouseMove(event) {
@@ -44,15 +56,72 @@
 
 		endPosition = getClientOffset(event);
 		drawLine();
+		lineHistory[lineHistory.length - 1].push(endPosition);
 		startPosition = endPosition;
 	}
 
-	function onMouseUp(event) {
+	function onMouseUp() {
 		isDrawing = false;
 	}
 
 	function clearCanvas() {
+		lineHistory = [];
 		ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+	}
+
+	function analyse() {
+		ctx.strokeStyle = 'red';
+
+		for (const line of lineHistory) {
+			const start = line[0];
+			const end = line[line.length - 1];
+
+			const rightMostPoint = start.x > end.x ? start : end;
+			const leftMostPoint = start.x < end.x ? start : end;
+
+			const gradient =
+				(rightMostPoint.y - leftMostPoint.y) /
+				(rightMostPoint.x - leftMostPoint.x);
+			const yIntercept = start.y - gradient * start.x;
+
+			if (isNaN(gradient)) {
+				// draw a vertical guideline
+				drawLine({
+					start: { x: start.x, y: 0 },
+					end: { x: start.x, y: canvasSize.height },
+				});
+			} else {
+				// find the two points that intersect the border
+				// of the canvas, and draw guideline between
+				const xZeroPoint = { x: 0, y: yIntercept };
+				const xWidthPoint = {
+					x: canvasSize.width,
+					y: gradient * canvasSize.width + yIntercept,
+				};
+				const yZeroPoint = { x: (-1 * yIntercept) / gradient, y: 0 };
+				const yHeightPoint = {
+					x: (canvasSize.height - yIntercept) / gradient,
+					y: canvasSize.height,
+				};
+
+				const pointsInBounds = [
+					xZeroPoint,
+					xWidthPoint,
+					yZeroPoint,
+					yHeightPoint,
+				].filter(
+					(point) =>
+						point.x >= 0 &&
+						point.x <= canvasSize.width &&
+						point.y >= 0 &&
+						point.y <= canvasSize.height
+				);
+
+				drawLine({ start: pointsInBounds[0], end: pointsInBounds[1] });
+			}
+		}
+
+		ctx.strokeStyle = '#000';
 	}
 
 	onMount(() => {
@@ -67,10 +136,10 @@
 <main>
 	<div>
 		<button class="select-none" on:click={clearCanvas}>Clear</button>
+		<button class="select-none" on:click={analyse}>Analyse</button>
 	</div>
-	<div class="artboard">
+	<div class="artboard select-none">
 		<canvas
-			class="select-none"
 			bind:this={canvasEl}
 			on:mousedown={onMouseDown}
 			on:touchstart={onMouseDown}
